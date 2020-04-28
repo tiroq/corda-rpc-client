ed145e836e7adddd47947b2ba85605bd275a49b3
ishamrai <isshamray@gmail.com>
2020-04-28 14:57:55 +0300

    UPD: start using dependencies from yaml file

--- rpc-client.py
+++ rpc-client.py
@@ -3,9 +3,13 @@
 
 import os
 import sys
+import yaml
 import logging
 import requests
 import optparse
+import importlib
+
+__version__ = '0.2.1'
 
 # Setup logging format for the file
 logging.basicConfig(
@@ -16,38 +20,6 @@ logging.basicConfig(
 logging.getLogger().addHandler(logging.StreamHandler())
 
 artifactory_url = 'https://ci-artifactory.corda.r3cev.com/artifactory/corda-releases/net/corda/corda-rpc/{version}/corda-rpc-{version}.jar'
-deps = {
-    'kotlin-stdlib-1.3.71.jar' : 'https://repo1.maven.org/maven2/org/jetbrains/kotlin/kotlin-stdlib/1.3.71/kotlin-stdlib-1.3.71.jar',
-    'corda-serialization-{version}.jar': 'https://ci-artifactory.corda.r3cev.com/artifactory/corda-releases/net/corda/corda-serialization/{version}/corda-serialization-{version}.jar',
-    'corda-node-api-{version}.jar': 'https://ci-artifactory.corda.r3cev.com/artifactory/corda-releases/net/corda/corda-node-api/{version}/corda-node-api-{version}.jar',
-    'corda-core-{version}.jar' : 'https://ci-artifactory.corda.r3cev.com/artifactory/corda-releases/net/corda/corda-core/{version}/corda-core-{version}.jar',
-    'corda-rpc-{version}.jar': 'https://ci-artifactory.corda.r3cev.com/artifactory/corda-releases/net/corda/corda-rpc/{version}/corda-rpc-{version}.jar',
-    'corda-{version}.jar': 'https://ci-artifactory.corda.r3cev.com/artifactory/corda-releases/net/corda/corda/{version}/corda-{version}.jar',
-    'corda-finance-contracts-{version}.jar': 'https://ci-artifactory.corda.r3cev.com/artifactory/corda-releases/net/corda/corda-finance-contracts/{version}/corda-finance-contracts-{version}.jar',
-    'corda-finance-workflows-{version}.jar': 'https://ci-artifactory.corda.r3cev.com/artifactory/corda-releases/net/corda/corda-finance-workflows/{version}/corda-finance-workflows-{version}.jar',
-    'guava-28.2-jre.jar': 'https://repo1.maven.org/maven2/com/google/guava/guava/28.2-jre/guava-28.2-jre.jar',
-    'slf4j-api-2.0.0-alpha1.jar': 'https://repo1.maven.org/maven2/org/slf4j/slf4j-api/2.0.0-alpha1/slf4j-api-2.0.0-alpha1.jar',
-    'caffeine-2.8.1.jar': 'https://repo1.maven.org/maven2/com/github/ben-manes/caffeine/caffeine/2.8.1/caffeine-2.8.1.jar',
-    'classgraph-4.8.69.jar': 'https://repo1.maven.org/maven2/io/github/classgraph/classgraph/4.8.69/classgraph-4.8.69.jar',
-    # 'proton-j-0.7.jar': 'https://repo1.maven.org/maven2/org/apache/qpid/proton-j/0.7/proton-j-0.7.jar',
-    'proton-j-0.33.4.jar': 'https://repo1.maven.org/maven2/org/apache/qpid/proton-j/0.33.4/proton-j-0.33.4.jar',
-    'artemis-core-client-2.11.0.jar': 'https://repo1.maven.org/maven2/org/apache/activemq/artemis-core-client/2.11.0/artemis-core-client-2.11.0.jar',
-    'javax.json-api-1.1.4.jar': 'https://repo1.maven.org/maven2/javax/json/javax.json-api/1.1.4/javax.json-api-1.1.4.jar',
-    'artemis-commons-2.11.0.jar': 'https://repo1.maven.org/maven2/org/apache/activemq/artemis-commons/2.11.0/artemis-commons-2.11.0.jar',
-    'jboss-logging-3.4.1.Final.jar': 'https://repo1.maven.org/maven2/org/jboss/logging/jboss-logging/3.4.1.Final/jboss-logging-3.4.1.Final.jar',
-    'commons-lang3-3.10.jar': 'https://repo1.maven.org/maven2/org/apache/commons/commons-lang3/3.10/commons-lang3-3.10.jar',
-    'eddsa-0.3.0.jar': 'https://repo1.maven.org/maven2/net/i2p/crypto/eddsa/0.3.0/eddsa-0.3.0.jar',
-    'bcprov-jdk15on-1.65.jar': 'https://repo1.maven.org/maven2/org/bouncycastle/bcprov-jdk15on/1.65/bcprov-jdk15on-1.65.jar',
-    # 'org.apache.servicemix.bundles.bcprov-jdk16-1.46_3.jar': 'https://repo1.maven.org/maven2/org/apache/servicemix/bundles/org.apache.servicemix.bundles.bcprov-jdk16/1.46_3/org.apache.servicemix.bundles.bcprov-jdk16-1.46_3.jar',
-    'commons-beanutils-1.9.4.jar': 'https://repo1.maven.org/maven2/commons-beanutils/commons-beanutils/1.9.4/commons-beanutils-1.9.4.jar',
-    'commons-logging-1.2.jar': 'https://repo1.maven.org/maven2/commons-logging/commons-logging/1.2/commons-logging-1.2.jar',
-    'kotlin-reflect-1.3.72.jar': 'https://repo1.maven.org/maven2/org/jetbrains/kotlin/kotlin-reflect/1.3.72/kotlin-reflect-1.3.72.jar',
-    # 'concrete-rx-observable-0.4.1.jar': 'https://repo1.maven.org/maven2/org/coodex/concrete-rx-observable/0.4.1/concrete-rx-observable-0.4.1.jar'
-    'rxjava-1.0.2.jar': 'https://repo1.maven.org/maven2/io/reactivex/rxjava/1.0.2/rxjava-1.0.2.jar',
-    # 'netty-all-5.0.0.Alpha2.jar': 'https://repo1.maven.org/maven2/io/netty/netty-all/5.0.0.Alpha2/netty-all-5.0.0.Alpha2.jar',
-    'netty-all-4.1.49.Final.jar': 'https://repo1.maven.org/maven2/io/netty/netty-all/4.1.49.Final/netty-all-4.1.49.Final.jar',
-    'failureaccess-1.0.jar': 'https://repo1.maven.org/maven2/com/google/guava/failureaccess/1.0/failureaccess-1.0.jar'
-}
 
 def main():
     options = optparse.OptionParser(usage='%prog [options]', description='corda rpc injector')
@@ -56,12 +28,8 @@ def main():
     options.add_option('--username', type='str', default='corda', help='username')
     options.add_option('--password', type='str', default='password', help='password')
     options.add_option('--version', type='str', default='4.4', help='corda version')
-    # options.add_option('--action', type='str', default='cash_issue_flow', help='action')
+    options.add_option('--dependencies_config', type='str', default='.gdeps.yaml', help='file with all required dependencies')
     # options.add_option('--amount', type='float', default=55.00, help='amount')
-    # options.add_option('--currency', type='str', default='USD', help='currency')
-    # options.add_option('--recipient', type='str', default='Party2', help='recipient')
-    # options.add_option('--issuer_ref', type='str', default='1234', help='issuer_ref')
-    # options.add_option('--notary', type='str', default='Notary', help='notary')
 
     opts, args = options.parse_args()
 
@@ -79,36 +47,38 @@ def main():
         os.mkdir(jlibs)
 
     # add library check
-    for jar_name in deps:
+    deps = yaml.load(open(opts.dependencies_config))
+    logging.info("Required {0} dependencies. Start loading them ...".format(len(deps['libs'])))
+    for dependency in deps['libs']:
+        jar_name = dependency['name']
+        logging.debug('Start working with dependency: {0}'.format(dependency))
         full_jar_name = jar_name.format(**params)
         jlib = os.path.join(jlibs, full_jar_name)
         if not os.path.exists(jlib):
-            url = deps[jar_name].format(**params)
+            url = dependency['link'].format(**params)
             logging.info("Download {0} lib".format(full_jar_name))
             with open(jlib, 'wb') as lib_file:
                 lib_file.write(requests.get(url).content)
+        else:
+            logging.debug('Already downloaded: {0}'.format(full_jar_name))
         sys.path.append(jlib)
-    from com.google.common.net import HostAndPort
-    from net.corda.client.rpc import CordaRPCClient
-    from net.corda.core.utilities import NetworkHostAndPort
-
-    from net.corda.finance.contracts.asset import Cash
-    from net.corda.core.node.services.vault import QueryCriteria
-    from net.corda.core.node.services.vault import PageSpecification
-    from net.corda.core.node.services.vault import Sort
-
-    import net.corda.finance.flows.CashIssueFlow
-    import net.corda.finance.flows.CashPaymentFlow
-
+        if 'class' in dependency:
+            _classes = dependency['class']
+            classes = []
+            if isinstance(_classes, basestring):
+                classes = [_classes]
+            elif isinstance(_classes, list):
+                classes = _classes
+            for _class in classes:
+                class_name = _class.split('.')[-1]
+                logging.info('Loading {0} class.'.format(_class))
+                globals()[class_name] = __import__(_class.rsplit('.', 1)[0], {}, {}, [class_name], 0)
 
     rpcAddress = NetworkHostAndPort(opts.hostname, opts.port)
     logging.info("Connecting to {0}".format(rpcAddress))
-    # client = CordaRPCClient(HostAndPort.fromString('{host}:{port}'.format(**params)), None, None)
     client = CordaRPCClient(rpcAddress)
     logging.info("RPC client created. Starting Auth process")
     proxy = client.start(opts.username, opts.password).proxy
-    # logging.info("Getting proxy object")
-    # proxy = client.proxy
     print dir(proxy)
     # txs = proxy.verifiedTransactions().first
     vault = proxy.vaultQueryBy(QueryCriteria.VaultQueryCriteria(), PageSpecification(), Sort([]), Cash.State).states
